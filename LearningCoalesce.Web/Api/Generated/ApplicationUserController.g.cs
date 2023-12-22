8e15b77e7addc7babac12bf12b0b55c75d8556d7
AndrewKorchemniy <andrewkorch@gmail.com>
2023-12-22 15:30:19 -0800

    init

---
+++ LearningCoalesce.Web/Api/Generated/ApplicationUserController.g.cs
@@ -0,0 +1,73 @@
+
+using IntelliTect.Coalesce;
+using IntelliTect.Coalesce.Api;
+using IntelliTect.Coalesce.Api.Controllers;
+using IntelliTect.Coalesce.Api.DataSources;
+using IntelliTect.Coalesce.Mapping;
+using IntelliTect.Coalesce.Mapping.IncludeTrees;
+using IntelliTect.Coalesce.Models;
+using IntelliTect.Coalesce.TypeDefinition;
+using LearningCoalesce.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace LearningCoalesce.Web.Api
+{
+    [Route("api/ApplicationUser")]
+    [Authorize]
+    [ServiceFilter(typeof(IApiActionFilter))]
+    public partial class ApplicationUserController
+        : BaseApiController<LearningCoalesce.Data.Models.ApplicationUser, ApplicationUserDtoGen, LearningCoalesce.Data.AppDbContext>
+    {
+        public ApplicationUserController(CrudContext<LearningCoalesce.Data.AppDbContext> context) : base(context)
+        {
+            GeneratedForClassViewModel = context.ReflectionRepository.GetClassViewModel<LearningCoalesce.Data.Models.ApplicationUser>();
+        }
+
+        [HttpGet("get/{id}")]
+        [Authorize]
+        public virtual Task<ItemResult<ApplicationUserDtoGen>> Get(
+            int id,
+            DataSourceParameters parameters,
+            IDataSource<LearningCoalesce.Data.Models.ApplicationUser> dataSource)
+            => GetImplementation(id, parameters, dataSource);
+
+        [HttpGet("list")]
+        [Authorize]
+        public virtual Task<ListResult<ApplicationUserDtoGen>> List(
+            ListParameters parameters,
+            IDataSource<LearningCoalesce.Data.Models.ApplicationUser> dataSource)
+            => ListImplementation(parameters, dataSource);
+
+        [HttpGet("count")]
+        [Authorize]
+        public virtual Task<ItemResult<int>> Count(
+            FilterParameters parameters,
+            IDataSource<LearningCoalesce.Data.Models.ApplicationUser> dataSource)
+            => CountImplementation(parameters, dataSource);
+
+        [HttpPost("save")]
+        [Authorize]
+        public virtual Task<ItemResult<ApplicationUserDtoGen>> Save(
+            [FromForm] ApplicationUserDtoGen dto,
+            [FromQuery] DataSourceParameters parameters,
+            IDataSource<LearningCoalesce.Data.Models.ApplicationUser> dataSource,
+            IBehaviors<LearningCoalesce.Data.Models.ApplicationUser> behaviors)
+            => SaveImplementation(dto, parameters, dataSource, behaviors);
+
+        [HttpPost("delete/{id}")]
+        [Authorize]
+        public virtual Task<ItemResult<ApplicationUserDtoGen>> Delete(
+            int id,
+            IBehaviors<LearningCoalesce.Data.Models.ApplicationUser> behaviors,
+            IDataSource<LearningCoalesce.Data.Models.ApplicationUser> dataSource)
+            => DeleteImplementation(id, new DataSourceParameters(), dataSource, behaviors);
+    }
+}
